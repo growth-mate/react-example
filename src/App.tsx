6a326263846641b681f7be0b4763b29413601da4
mohamedalichelbi <mohamedalichelbi123@gmail.com>
2024-09-02 14:05:19 +0200

    Fix: change page title, favicons, and format project files with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,46 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { AdPage, FeedPage } from "./routes";
+import React from "react";
 
-const App = () => (
-	<Routes>
-		<Route
-			path="/ads"
-			element={<AdPage />}
-		/>
-		<Route
-			path="/feeds"
-			element={<FeedPage />}
-		/>
-		<Route
-			index
-			path="*"
-			element={
-				<Navigate
-					to="/feeds"
-					replace
-				/>
-			}
-		/>
-	</Routes>
-);
+const App = () => {
+	// Enable Amplitude
+	React.useEffect(() => {
+		// Amplitude analytics
+		if (Object.prototype.hasOwnProperty.call(window, "amplitude")) {
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			window.amplitude.add(window.sessionReplay.plugin({ sampleRate: 1 })).promise.then(function () {
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-ignore
+				window.amplitude.init("344a531fcd2975dbd1de0a38aa328371", {
+					autocapture: { elementInteractions: true, sessions: true },
+				});
+			});
+		}
+	}, []);
+
+	return (
+		<Routes>
+			<Route
+				path="/ads"
+				element={<AdPage />}
+			/>
+			<Route
+				path="/feeds"
+				element={<FeedPage />}
+			/>
+			<Route
+				index
+				path="*"
+				element={
+					<Navigate
+						to="/feeds"
+						replace
+					/>
+				}
+			/>
+		</Routes>
+	);
+};
 
 export default App;
