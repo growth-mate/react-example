c40b99ef056218933e0b0310e37cfe90fab15145
CreedsCode <contact@creedscode.cc>
2024-10-09 02:36:00 +0200

    Adding mobile support

--- src/routes/FeedPage.tsx
+++ src/routes/FeedPage.tsx
@@ -20,28 +20,98 @@ export const FeedPage: React.FC = () => {
 				<div className="flex-column">
 					<br />
 					<h2>
-						{walletSelector && walletSelector?.accountId != null
-							? `GM, ${walletSelector!.accountId}!`
+						{accountId != null
+							? `GM, ${accountId.length > 10 ? truncateAccountId(accountId) : accountId}!`
 							: "Curated Actions for You"}
 					</h2>
 					<div>
 						Discover the latest offers and news in your ecosystem based on your transaction history. Log in
 						now and stay up to date! 🚀
 					</div>
-					<button
-						className="connect-wallet"
-						onClick={async () => {
-							if (walletSelector?.accountId != null) (await walletSelector!.selector.wallet()).signOut();
-							else walletSelector!.modal.show();
-						}}
-					>
-						{walletSelector && walletSelector?.accountId != null ? <p>Log Out</p> : <p>Connect Wallet</p>}
-					</button>
+					<div className="ecosystem-selector">
+						<div>
+							<label htmlFor="ecosystem-select">
+								<h2>Select your Ecosystem</h2>
+							</label>
+							<select
+								id="ecosystem-select"
+								aria-label="Select your Ecosystem"
+								onChange={(e) => {
+									if (e.target.value == "Near") {
+										setAccountId(null);
+									}
+									return setSelectedEcosystem(
+										Ecosystems.find((ecosystem) => ecosystem.name === e.target.value)!
+									);
+								}}
+							>
+								{Ecosystems.map((ecosystem) => (
+									<option
+										key={ecosystem.name}
+										value={ecosystem.name}
+									>
+										{ecosystem.name}
+									</option>
+								))}
+							</select>
+						</div>
+						<div>
+							<div className="ecosystem-selector">
+								{selectedEcosystem.name == "Near" ? (
+									<div>
+										<input
+											className="ecosystem-selector-input-disabled"
+											type="text"
+											disabled={true}
+											placeholder="Enter your Wallet Address"
+											onChange={(e) => setInputAccountId(e.target.value)}
+										/>
+										<button
+											className="connect-wallet"
+											onClick={async () => {
+												if (walletSelector?.accountId != null)
+													(await walletSelector!.selector.wallet()).signOut();
+												else walletSelector!.modal.show();
+											}}
+										>
+											{walletSelector && walletSelector?.accountId != null ? (
+												<p>Log Out</p>
+											) : (
+												<p>Connect Wallet</p>
+											)}
+										</button>
+									</div>
+								) : (
+									<div>
+										<input
+											className="ecosystem-selector-input"
+											type="text"
+											placeholder="Enter your Wallet Address"
+											onChange={(e) => setInputAccountId(e.target.value)}
+										/>
+										<button
+											className="connect-wallet"
+											onClick={() => {
+												setAccountId(inputAccountId);
+												if (inputAccountId.length == 0) {
+													setAccountId(null);
+												}
+											}}
+										>
+											Load Feed
+										</button>
+									</div>
+								)}
+							</div>
+						</div>
+					</div>
 					<br />
 					<br />
 					<div
 						style={
-							searchParams.has("unblur") || (walletSelector && walletSelector?.accountId != null)
+							searchParams.has("unblur") ||
+							(walletSelector && walletSelector?.accountId != null) ||
+							accountId != null
 								? {}
 								: { filter: "blur(10px)" }
 						}
@@ -137,6 +207,7 @@ export const FeedPage: React.FC = () => {
 										<input
 											className="ecosystem-selector-input"
 											type="text"
+											disabled={selectedEcosystem.name == "Near"}
 											placeholder="Enter your Wallet Address"
 											onChange={(e) => setInputAccountId(e.target.value)}
 										/>
